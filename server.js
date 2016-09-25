64ab670da3b0ef594a64baeed31b1504c99dfee4
Vineesh KP <vineeshkp7@gmail.com>
2016-09-25 12:54:04 +0530

    Final commit on pages

--- server.js
+++ server.js
@@ -5,20 +5,133 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+'article-one': {
+  title: 'Article One | Vineesh KP',
+  heading: 'Article One',
+  date: 'Sep 5 2016',
+  content:`
+   <p>
+     In this article we are going to talk about server side programming to make urls.
+   </p>
+   <p>
+     In this article we are going to talk about server side programming to make urls.
+   </p>
+   <p>
+     In this article we are going to talk about server side programming to make urls.
+   </p>
+   <p>
+     In this article we are going to talk about server side programming to make urls.
+   </p>
+   <p>
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+   </p>
+   <p>
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+   </p>`
+ },
+
+ 'article-two': {
+   title: 'Article Two | Vineesh KP',
+   heading: 'Article Two',
+   date: 'Sep 10 2016',
+   content:`
+    <p>
+      In this article we are going to talk about server side programming to make urls.
+    </p>
+    <p>
+      In this article we are going to talk about server side programming to make urls.
+    </p>
+    <p>
+      In this article we are going to talk about server side programming to make urls.
+    </p>
+    <p>
+      In this article we are going to talk about server side programming to make urls.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>`
+  },
+
+
+  'article-three': {
+    title: 'Article Three | Vineesh KP',
+    heading: 'Article Three',
+    date: 'Sep 15 2016',
+    content:`
+    <p>
+      In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.
+    </p>
+    <p>
+      In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.
+    </p>
+    <p>
+      In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.
+    </p>
+    <p>
+      In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.In this article we are going to talk about client side javascript.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>`
+   }
+
+};
+
+function createTemplate (data) {
+  var title = data.title;
+  var heading = data.heading;
+  var date = data.date;
+  var content = data.content;
+
+var htmlTemplate = `
+<html>
+ <head>
+   <title>
+     ${title}
+   </title>
+     <meta name="viewpoint" content="width=device-width, initial-scale=1" />
+     <link href="/ui/style.css" rel="stylesheet" />
+   <body>
+     <div class="container">
+       <div>
+         <a href="/">Home</a>
+       </div>
+       <hr/>
+       <h1>
+         ${heading}
+       </h1>
+       <div>
+         ${date}
+       </div>
+
+       <div>
+         ${content}
+       </div>
+
+     </div>
+   </body>
+ </head>
+</html>
+`;
+return htmlTemplate
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+//  articleName == article-one
+//  articles[articleName] == {} content object for article-one
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/background.jpg', function (req, res) {
@@ -27,6 +140,10 @@ app.get('/ui/background.jpg', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/home.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'home.css'));
 });
 
 app.get('/ui/vini.png', function (req, res) {
