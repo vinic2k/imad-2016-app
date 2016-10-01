f03afe0c54f5fc4aa0955fbe3dc89a2683aad6a4
vinic2k <vineeshkp7@gmail.com>
2016-10-01 10:38:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 // Change content of main-text
 var element = document.getElementById('main-text');
-
 element.innerHTML = 'New Value';
+
+// Move the image
+var img = doculment.getElementById('vini');
+img.onClick = function (){
+    img.style.marginLeft = "100px";
+}
