0f2d41f77e8f360a83cb9018f795b2eafc396128
vinic2k <vineeshkp7@gmail.com>
2016-10-01 11:00:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var marginLeft = 0;
 function moveRight (){
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
-};
+}
 var img = document.getElementById('vini');
 img.onclick = function (){
   var interval = setInterval(moveRight, 1);
