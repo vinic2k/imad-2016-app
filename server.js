e25280a1998b651c8ff64ac37d9cd3b4838d63e7
vinic2k <vineeshkp7@gmail.com>
2016-10-01 10:13:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,6 +146,10 @@ app.get('/ui/home.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'home.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/vini.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'vini.png'));
 });
