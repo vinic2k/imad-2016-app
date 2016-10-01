a2a35915062a719af262ee969bb3e5f77fd6be8d
vinic2k <vineeshkp7@gmail.com>
2016-10-01 10:57:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 // Move the image
+var marginLeft = 0;
+function moveRight (){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+};
 var img = document.getElementById('vini');
 img.onclick = function (){
-    img.style.marginLeft = '100px';
-};
+  var interval = setInterval(moveRight, 10);
+};v
