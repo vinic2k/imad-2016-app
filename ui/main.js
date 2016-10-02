95b178615bd969bffcbdbd888f8880e9146c238a
vinic2k <vineeshkp7@gmail.com>
2016-10-02 19:56:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 // Change content of main-text
 var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
+element.innerHTML = 'My name is Vineesh KP. This is the webapp I created for the Introduction to Modern Application Development course by Hasura.io and NPTEL. Thank you for visiting my page';
 
 // Move the image
 var marginLeft = 0;
