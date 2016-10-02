ab60ad63f9023c64c9d85e67f07db0fa7354d683
vinic2k <vineeshkp7@gmail.com>
2016-10-02 22:17:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var couter = 0;
+var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
