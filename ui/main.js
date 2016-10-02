a81049d10d2d45d238251dc97f8894f5a1450c6e
vinic2k <vineeshkp7@gmail.com>
2016-10-02 22:53:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,14 +15,24 @@ console.log('Loaded!');
 // };
 // Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function (){
-  // Make a request to the counter endpoint
-  
-  // Capture the response and store it in a variable
-  
-  // Render the variable in the correct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+      // Create a request object
+      var request = new XMLHttpRequest();
+      
+      // Capture the response and store it in a variable
+      request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE) {
+            // Take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        
+        }
+        // Not done yet; no action taken
+        request.open('GET', 'http://vinic2k.imad.hasura-app.io/counter', true);
+        request.send(null);
+      };
+
 };
