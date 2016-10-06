c7f407a9fa895a0d6ff2817194afea4c172f3102
vinic2k <vineeshkp7@gmail.com>
2016-10-06 17:42:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick = function () {
             if(request.status === 200) {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                span.innerHTML = count.toString();
             }
         
         }
