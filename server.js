fddcea7f495e5d7e37a19293181524fadf2529f3
vinic2k <vineeshkp7@gmail.com>
2016-11-07 12:29:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'vinic2k',
+    database: 'vinic2k',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -125,6 +134,19 @@ return htmlTemplate
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+   // Maka a select request
+   // Return a response with the results
+   pool.query('SELECT *  FROM test', function(err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       }else {
+           res.send(JSON.stringify(result));
+       }
+   });
 });
 
 var counter = 0;
