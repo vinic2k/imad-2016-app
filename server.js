1c1fcf37b03eb846e1cefaf50a6eccbc62fc4bb6
vinic2k <vineeshkp7@gmail.com>
2016-11-07 14:37:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -165,11 +165,22 @@ app.get('/submit-name', function (req, res) { // URL: /submit-name?name=Elliot
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
 //  articleName == article-one
 //  articles[articleName] == {} content object for article-one
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+
+  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(404).send('Article not found');
+          } else {
+              articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+  });
 });
 
 app.get('/ui/background.jpg', function (req, res) {
