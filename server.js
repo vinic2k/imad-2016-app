cc875238ae4e9ddc157915e915e4bef95f6b2825
vinic2k <vineeshkp7@gmail.com>
2016-11-07 10:58:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -160,6 +160,15 @@ app.get('/ui/vini.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'vini.png'));
 });
 
+var names = [];
+app.get('/submit-name/:name', fuction(req, res) {
+    // Get the name from the request
+    var name = req.params.name;
+    
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
