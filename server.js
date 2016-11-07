8be60bd4059bec5e44a8c08ba771ebc0077aa658
vinic2k <vineeshkp7@gmail.com>
2016-11-07 11:05:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,6 +133,16 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function (req, res) { // URL: /submit-name?name=Elliot
+    // Get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res) {
 //  articleName == article-one
 //  articles[articleName] == {} content object for article-one
@@ -160,15 +170,6 @@ app.get('/ui/vini.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'vini.png'));
 });
 
-var names = [];
-app.get('/submit-name', function (req, res) { // URL: /submit-name?name=Elliot
-    // Get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
