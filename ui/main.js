e8d8ebafbbca36d4a33eaa90be66a6bf56d33c32
vinic2k <vineeshkp7@gmail.com>
2016-11-07 10:46:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,20 @@ button.onclick = function () {
   request.open('GET', 'http://vinic2k.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
+    // Make a request to the server and send the name
+    
+    // Capture the list of names and render it as a list
+    var names =['Darlene', 'Angela', 'Mobley'];
+    var list = '';
+    for (i=0; i< names.length; i++){
+        list += '<li>' + names[i] + '</li>'
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
