5644d07b48da02badac629007c8bcd707bc58c9d
vinic2k <vineeshkp7@gmail.com>
2016-11-07 14:40:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ app.get('/articles/:articleName', function (req, res) {
 //  articleName == article-one
 //  articles[articleName] == {} content object for article-one
 
-  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
