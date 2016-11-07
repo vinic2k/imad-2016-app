7fce573ffc2d7f6d386abe6f30528289046decbb
vinic2k <vineeshkp7@gmail.com>
2016-11-07 11:03:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -161,9 +161,9 @@ app.get('/ui/vini.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) { // URL: /submit-name?name=Elliot
     // Get the name from the request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     
