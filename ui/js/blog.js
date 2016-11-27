b03f698ea40195ca67e8d49d11d4017c3e320827
Vineesh KP <vineeshkp7@gmail.com>
2016-11-27 16:22:20 +0530

    some minor changes on the article page

--- ui/js/blog.js
+++ ui/js/blog.js
@@ -9,7 +9,7 @@ function loadArticles () {
                 var articleData = JSON.parse(this.responseText);
                 for (var i=0; i< articleData.length; i++) {
                     content += `<li>
-                    <a href="/articles/${articleData[i].link}"><h1>${articleData[i].heading}<small>By Vineesh KP</small></h1></a>
+                    <a href="/articles/${articleData[i].link}"><h1>${articleData[i].heading}<small> By Vineesh KP</small></h1></a>
                     (${articleData[i].date.split('T')[0]})
                     <br><p>${articleData[i].brief}</p>
                     <br><button class="btn btn-info"><a href="/articles/${articleData[i].link}">Read more</a></button></li><hr>`;
