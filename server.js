c3a0413bdeb8bbd20ae4cc75d4955eb4163ab013
vinic2k <vineeshkp7@gmail.com>
2016-12-04 04:42:21 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -177,6 +177,7 @@ function createTemplate (data) {
         <!-- Bootstrap JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <!-- Custom JavaScript -->
+        <script type="text/javascript" src="/ui/js/my.js"></script>
         <script type="text/javascript" src="/ui/js/article.js"></script>
       </body>
     </html>
